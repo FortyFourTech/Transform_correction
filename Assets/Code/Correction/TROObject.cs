0ec27c75f5f93eeba1ebd047450c613af615b96d
Dimar <fortyfourtech@gmail.com>
2018-01-07 22:53:47 +0700

    renamed function in TROObject

--- Assets/Code/Correction/TROObject.cs
+++ Assets/Code/Correction/TROObject.cs
@@ -2,9 +2,8 @@
 
 namespace Correction {
     public class TROObject : MonoBehaviour {
-        public float changeTransform(TransformField field, Axis axis, float val) {
-            return
-                gameObject.transform.AddValue(field, axis, val);
+        public float ChangeTransform(TransformField field, Axis axis, float val) {
+            return gameObject.transform.AddValue(field, axis, val);
         }
     }
 }
